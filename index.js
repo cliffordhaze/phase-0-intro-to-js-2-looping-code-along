06acee74f766176a152b93961acdd7406351bf05
Nicole Gomez <nicolegomezx3@gmail.com>
2023-03-08 16:53:12 -0500

    Adds looping and countdown

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+
+function writeCards(names, eventName) {
+    let messages = []
+    // names, eventName, messages, i
+
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    // messages.push("A")
+    return messages;
+}
+
+function countDown(int) {
+    while (int >= 0) {
+        console.log(int--)
+    }
+}
